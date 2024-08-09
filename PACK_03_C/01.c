12aa309eb65f0bbe4a26c150b1b139416c2dfad9
Moein <108075466+claxpoint@users.noreply.github.com>
2024-08-09 10:37:32 -0700

    Create 01.c

---
+++ PACK_03_C/01.c
@@ -0,0 +1,29 @@
+// Include necessary libraries
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define constants for SOCKS5/4 and MTProto
+#define SOCKS5 5
+#define SOCKS4 4
+#define MTProto 1
+
+// Function to establish a reverse TCP connection
+void establishReverseTCPConnection() {
+    // Code for establishing reverse TCP connection
+}
+
+// Function for MTProto & SOCKS5/4 tunneling
+void MTProtoSocksTunnel() {
+    // Code for MTProto & SOCKS5/4 tunneling
+}
+
+// Main function
+int main() {
+    // Call function to establish reverse TCP connection
+    establishReverseTCPConnection();
+
+    // Call function for MTProto & SOCKS5/4 tunneling
+    MTProtoSocksTunnel();
+
+    return 0;
+}
